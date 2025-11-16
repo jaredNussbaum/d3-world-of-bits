34773885dfb9a75bf0f5acda1c42e61a594cd987
jaredNussbaum <74433775+jaredNussbaum@users.noreply.github.com>
2025-11-16 23:28:54 +0000

    d3 a completed! added exponenital cells taht can be combined, added full map coverage, added player inventory

--- src/main.ts
+++ src/main.ts
@@ -24,9 +24,9 @@ document.body.append(mapDiv);
 const statusPanelDiv = document.createElement("div");
 statusPanelDiv.id = "statusPanel";
 document.body.append(statusPanelDiv);
-
+const RANGE = 6;
 // Our classroom location
-const CLASSROOM_LATLNG = leaflet.latLng(
+const PLAYER_POS = leaflet.latLng(
   36.997936938057016,
   -122.05703507501151,
 );
@@ -34,12 +34,12 @@ const CLASSROOM_LATLNG = leaflet.latLng(
 // Tunable gameplay parameters
 const GAMEPLAY_ZOOM_LEVEL = 19;
 const TILE_DEGREES = 1e-4;
-const NEIGHBORHOOD_SIZE = 8;
+const NEIGHBORHOOD_SIZE = 25;
 const CACHE_SPAWN_PROBABILITY = 0.1;
 
 // Create the map (element with id "map" is defined in index.html)
 const map = leaflet.map(mapDiv, {
-  center: CLASSROOM_LATLNG,
+  center: PLAYER_POS,
   zoom: GAMEPLAY_ZOOM_LEVEL,
   minZoom: GAMEPLAY_ZOOM_LEVEL,
   maxZoom: GAMEPLAY_ZOOM_LEVEL,
@@ -57,51 +57,65 @@ leaflet
   .addTo(map);
 
 // Add a marker to represent the player
-const playerMarker = leaflet.marker(CLASSROOM_LATLNG);
+const playerMarker = leaflet.marker(PLAYER_POS);
 playerMarker.bindTooltip("That's you!");
 playerMarker.addTo(map);
 
 // Display the player's points
-let playerPoints = 0;
-statusPanelDiv.innerHTML = "No points yet...";
+let current_num = 0;
+set_current_held_points(0);
 
-// Add caches to the map by cell numbers
 function spawnCache(i: number, j: number) {
   // Convert cell numbers into lat/lng bounds
-  const origin = CLASSROOM_LATLNG;
+  const origin = PLAYER_POS;
   const bounds = leaflet.latLngBounds([
     [origin.lat + i * TILE_DEGREES, origin.lng + j * TILE_DEGREES],
     [origin.lat + (i + 1) * TILE_DEGREES, origin.lng + (j + 1) * TILE_DEGREES],
   ]);
 
+  let cachePoints = Math.pow(
+    2,
+    Math.floor(luck([i, j, "initialValue"].toString()) * 4),
+  );
+
   // Add a rectangle to the map to represent the cache
   const rect = leaflet.rectangle(bounds);
   rect.addTo(map);
 
-  // Handle interactions with the cache
-  rect.bindPopup(() => {
-    // Each cache has a random point value, mutable by the player
-    let pointValue = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
+  // Display text on the cache
+  const tooltip = leaflet.tooltip({ permanent: true, direction: "center" })
+    .setContent(cachePoints.toString());
+  rect.bindTooltip(tooltip);
 
-    // The popup offers a description and button
-    const popupDiv = document.createElement("div");
-    popupDiv.innerHTML = `
-                <div>There is a cache here at "${i},${j}". It has value <span id="value">${pointValue}</span>.</div>
-                <button id="poke">poke</button>`;
-
-    // Clicking the button decrements the cache's value and increments the player's points
-    popupDiv
-      .querySelector<HTMLButtonElement>("#poke")!
-      .addEventListener("click", () => {
-        pointValue--;
-        popupDiv.querySelector<HTMLSpanElement>("#value")!.innerHTML =
-          pointValue.toString();
-        playerPoints++;
-        statusPanelDiv.innerHTML = `${playerPoints} points accumulated`;
-      });
-
-    return popupDiv;
+  // Click mouseevent
+  rect.on("click", () => {
+    if (distance_to(i, j) <= RANGE) {
+      if (current_num == 0) {
+        set_current_held_points(cachePoints);
+        rect.remove();
+      } else {
+        if (current_num == cachePoints) {
+          cachePoints *= 2;
+          tooltip.setContent(cachePoints.toString());
+          set_current_held_points(0);
+        } else {
+          set_current_held_points(0);
+        }
+      }
+    }
   });
+}
+function set_current_held_points(x: number) {
+  current_num = x;
+  if (current_num == 0) {
+    statusPanelDiv.innerHTML = "Currently Holding Nothing";
+    current_num = 0;
+    return;
+  }
+  statusPanelDiv.innerHTML = "Currently Holding: " + current_num;
+}
+function distance_to(i: number, j: number) {
+  return Math.sqrt((i ** 2) + (j ** 2));
 }
 
 // Look around the player's neighborhood for caches to spawn
